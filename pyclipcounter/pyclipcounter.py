f7edb7bcd42740de10c20e43b9292bf94b752a42
Carsten Engelke <carsten.engelke@gmail.com>
2025-03-01 09:27:49 +0100

    added ui using pygubu-designer

---
+++ pyclipcounter/pyclipcounter.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import pathlib
+import tkinter as tk
+import pygubu
+from pyclipcounterui import PyClipCounterUI
+
+
+class PyClipCounter(PyClipCounterUI):
+    def __init__(self, master=None):
+        super().__init__(master)
+
+
+if __name__ == "__main__":
+    app = PyClipCounter()
+    app.run()
